47dd6a78e294c6e3c89d86787a2aa5b4e91688e3
akshayaravindh1015 <akshaynetha1015@gmail.com>
2022-02-25 18:23:17 +0530

    Maximilan code

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -38,9 +38,7 @@ const ExpenseForm = (props) => {
     //   enteredDate: event.target.value,
     // });
   };
-  const cancelHanlder = () => {
-    props.onCancel(false);
-  }
+
   const submitHandler = (event) => {
     event.preventDefault();
 
@@ -89,7 +87,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
-        <button onClick={cancelHanlder}>Cancel</button>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
